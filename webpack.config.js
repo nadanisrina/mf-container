53fb1ff787c40a600c0867a5e3d0482c52d60dcd
nadans <nisrinaseptiana1@gmail.com>
2021-11-01 17:00:07 +0700

    initial container foldering

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+let HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: "8080",
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
